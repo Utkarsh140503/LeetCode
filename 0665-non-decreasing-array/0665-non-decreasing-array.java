bd3a09d075a67f335c2147bf9d2ab4c4dffcf78d
UTKARSH MISHRA <96302520+Utkarsh140503@users.noreply.github.com>
2025-02-22 22:17:48 +0530

    Time: 0 ms (100%), Space: 45.4 MB (54.91%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.java
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
-        int count = 0;
+        int count = 0; // Count of modifications
+
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] > nums[i + 1]) {
-                count++;
-                if (count > 1) {
-                    return false;
+                if (count == 1) {
+                    return false; // More than one modification required
                 }
-                if (i > 0 && nums[i - 1] > nums[i + 1]) {
-                    nums[i + 1] = nums[i];
+                count++;
+
+                // Modify nums[i] or nums[i+1] to maintain order
+                if (i == 0 || nums[i - 1] <= nums[i + 1]) {
+                    nums[i] = nums[i + 1]; // Adjust current element
                 } else {
-                    nums[i] = nums[i + 1];
+                    nums[i + 1] = nums[i]; // Adjust next element
                 }
             }
         }
